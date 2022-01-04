43ce7c5cdf641f2864ba2ac94f456fcee89f6f4a
Irfan <Irfan@DESKTOP-MT907FK>
2022-01-04 14:31:11 +0800

    as

--- src/gsc/receipt.java
+++ src/gsc/receipt.java
@@ -4,16 +4,17 @@
  */
 package gsc;
 
-import javax.swing.JOptionPane;
-import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter; 
+import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import javax.swing.JOptionPane;
+
 
 
 
